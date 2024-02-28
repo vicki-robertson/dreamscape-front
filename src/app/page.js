d117fed9d567f42091ca1e33f68c6f010b1f123e
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-02-28 11:37:22 +0100

    added tailwind utility classes for fonts

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Page() {
   return (
     <div>
-      <h1 className=''>Home</h1>
+      <h1 className='text-xs font-bold desktop:text-m'>Home</h1>
       <Link href="/about">About</Link>
     </div>
   )
