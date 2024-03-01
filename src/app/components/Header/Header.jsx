b7a7c67a4ed4b9102beda5826132d57cec2c61e4
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-03-01 17:40:02 +0100

    input box added to components

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,20 +1,15 @@
 import HomeButton from "@/app/components/ui/HomeButton";
-import InputBox from "@/app/components/ui/InputBox";
 import LoginButton from "@/app/components/ui/LoginButton";
 import Logo from "@/app/components/ui/Logo";
-import SearchBar from "@/app/components/ui/SearchBar";
 import SearchButton from "@/app/components/ui/SearchButton";
 
 
 export default function Header() {
     return (
-        <div>
+        <div className="">
             <Logo/>
             <HomeButton/>
             <LoginButton/>
-            <SearchButton/>
-            <InputBox/>
-            <SearchBar />
         </div>
     )
 }
---
+++ src/app/components/ui/SearchBar.jsx
@@ -0,0 +1,9 @@
+import InputBox from "./InputBox";
+
+export default function SearchBar() {
+    return (
+      <>
+        <InputBox boxHeight="[40px]" boxWidth="[200px]" />
+      </>
+    )
+  }
