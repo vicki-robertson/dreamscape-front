aec37cb08b142aec3b63bd8c999faf93e71072a7
lolamindi <laura_mday@yahoo.co.uk>
2024-03-01 15:03:49 +0100

    create basic photo card with text for non-logged-in users

--- src/app/components/ui/PhotoCard.jsx
+++ src/app/components/ui/PhotoCard.jsx
@@ -1,21 +1,22 @@
 import Image from "next/image";
-
+import palmTrees from "../../../../public/assets/icons/palm-trees.png";
 
 function PhotoCard() {
   return (
-    <article>
+    <article className="h-[373px] w-[300px] relative">
       <Image
-        className=
-        src=
-        width=
-        height=
-        alt= 
+        className="rounded-2xl left-0 top-0 absolute z-20"
+        src={palmTrees}
+        width={300}
+        height={300}
+        alt={"representación del destino eligido"}
       />
-      <h3 className=</h3>
-      <h3 className=</h3>
-      <br />
+      <div className=" flex flex-col h-[100px] w-[300px] left-0 top-[273px] bg-light-yellow rounded-br-[20px] rounded-bl-[20px] absolute">
+      </div>
+      <span className="h-11 w-[207px] left-[21px] top-[302px] absolute text-m font-bold text-blue">Islas Azores</span>
+      <span className="h-11 w-[207px] left-[21px] top-[329px] absolute text-s text-blue">Portugal</span>
     </article>
   );
 }
 
-export default DoctorCard;
+export default PhotoCard;
--- src/app/ui-pages/ui-login/page.jsx
+++ src/app/ui-pages/ui-login/page.jsx
@@ -1,8 +1,14 @@
 import DoubleButton from "@/app/components/ui/DoubleButton";
+import PhotoCard from "@/app/components/ui/PhotoCard";
 
 function TestPage() {
   return (
+    <>
+    <br />
     <DoubleButton/>
+    <br />
+    <PhotoCard/>
+    </>
   );
 }
 
