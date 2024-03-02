f25a7c9876a4172e07ba060dbea16dc2f849aa67
lolamindi <laura_mday@yahoo.co.uk>
2024-03-03 00:21:43 +0100

    create details page for non logged-in users

--- src/app/ui-pages/ui-login/page.jsx
+++ src/app/ui-pages/ui-login/page.jsx
@@ -1,10 +1,28 @@
 import Header from "@/app/components/Header/Header";
-import PhotoCard from "@/app/components/ui/PhotoCard";
+import PalmTrees from "../../../../public/assets/icons/palm-trees.png";
+import Image from "next/image";
 
 function TestPage() {
   return (
     <>
-      <Header />
+      <article className=" flex flex-col m-auto desktop:w-[900px] h-[450px] desktop:flex-row desktop:gap-10 desktop:my-20">
+        <Image
+          className="rounded-br-[20px] rounded-bl-[20px]"
+          src={PalmTrees}
+          width={450}
+          height={450}
+          alt={"representación del destino eligido"}
+        />
+        <section className="flex flex-col m-3">
+          <h3 className="text-l font-bold text-red ">
+            Islas Azores
+          </h3>
+          <h4 className="text-m mt-[-14px] mb-4 text-red">Portugal</h4>
+          <p className="text-s text-blue">
+          Lorem ipsum dolor sit amet, consectetur farit adipiscing elit. Sed do eiusmod tempor colisi incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercit in ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in repre van henderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          </p>
+        </section>
+      </article>
     </>
   );
 }
