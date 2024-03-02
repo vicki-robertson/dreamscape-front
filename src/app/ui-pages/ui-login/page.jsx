07d163087b73970b8073751a6370fa553c1beae9
lolamindi <laura_mday@yahoo.co.uk>
2024-03-02 22:25:19 +0100

    enter grid with photo cards into home page

--- src/app/ui-pages/ui-login/page.jsx
+++ src/app/ui-pages/ui-login/page.jsx
@@ -5,15 +5,6 @@ function TestPage() {
   return (
     <>
       <Header />
-      <article className="flex justify-center items-center h-full">
-        <div className="grid grid-cols-1 desktop:grid-cols-4 gap-x-6 gap-y-6 my-6 desktop:mx-16">
-          {[...Array(8).keys()].map((index) => (
-            <div key={index} className="flex justify-center">
-              <PhotoCard />
-            </div>
-          ))}
-        </div>
-      </article>
     </>
   );
 }
