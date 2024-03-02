edb30e735042cebb007069ff7c80b0a3faa95b0d
lolamindi <laura_mday@yahoo.co.uk>
2024-03-02 01:08:54 +0100

    create card for logged-in users

--- src/app/components/ui/LoggedCard.jsx
+++ src/app/components/ui/LoggedCard.jsx
@@ -1,16 +1,24 @@
 import Image from "next/image";
-import palmTrees from "../../../../public/assets/icons/palm-trees.png";
+import PalmTrees from "../../../../public/assets/icons/palm-trees.png";
+import QuestionMark from "../../../../public/assets/icons/Info-icon.svg";
 import EditDeleteIcons from "./EditDeleteIcons";
 
 function LoggedCard() {
   return (
     <article className="h-[373px] w-[300px] relative">
       <Image
-        className="rounded-2xl left-0 top-0 absolute z-20 opacity-50"
-        src={palmTrees}
+        className="rounded-2xl left-0 top-0 absolute z-20"
+        src={PalmTrees}
         width={300}
         height={300}
         alt={"representación del destino eligido"}
+      />
+      <Image
+        className="absolute right-2 top-2 z-30"
+        src={QuestionMark}
+        width={48}
+        height={48}
+        alt={"signo de interrogación"}
       />
       <div className=" flex flex-col h-[100px] w-[300px] left-0 top-[273px] bg-light-yellow rounded-br-[20px] rounded-bl-[20px] absolute"></div>
       <span className="h-11 w-[207px] left-[21px] top-[302px] absolute text-m font-bold text-blue">
