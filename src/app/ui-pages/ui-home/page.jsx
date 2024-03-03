6d65878a6cd2b25587353af5cb880308a70f7c63
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-03-03 11:28:53 +0100

    pagination buttons added, without logic to change page

--- src/app/ui-pages/ui-home/page.jsx
+++ src/app/ui-pages/ui-home/page.jsx
@@ -1,6 +1,7 @@
 import NavbarMobile from "@/app/components/Header/NavbarMobile";
 import Header from "@/app/components/Header/Header";
 import Modal from "@/app/components/ui/Modal";
+import PaginationButtons from "@/app/components/ui/PaginationButtons";
 
 export default function Home() {
     return (
@@ -9,6 +10,7 @@ export default function Home() {
             <NavbarMobile />
             <div className="flex flex-row justify-center">
                 <Modal/>
+                <PaginationButtons/>
             </div>
         </>
     )
