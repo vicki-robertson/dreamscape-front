e5497b365c6cf639108d01c4129a4e58e861d3de
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-03-03 11:33:49 +0100

    Merge branch 'dev' of https://github.com/vicki-robertson/dreamscape-front into feature-tailwind-config
    Co-authored-by: Laura Artaza <lolamindi@users.noreply.github.com>

---
+++ src/app/components/ui/PaginationButtons.jsx
@@ -0,0 +1,29 @@
+
+import React from 'react'
+import prevButton from '../../../../public/assets/icons/Previous-button.svg';
+import nextButton from '../../../../public/assets/icons/Next-button.svg';
+import Image from 'next/image';
+
+export default function PaginationButtons() {
+  return (
+    <div className='flex flex-row gap-6 items-center'>
+        <a>
+            <Image 
+                src={prevButton}
+                alt="previous page"
+                height={40}
+                width={60}
+            />      
+        </a>
+        <p className='text-m text-blue font-bold'>1</p>
+        <a>
+            <Image 
+                src={nextButton}
+                alt="next page"
+                height={40}
+                width={60}
+            />      
+        </a>
+    </div>
+  )
+}
