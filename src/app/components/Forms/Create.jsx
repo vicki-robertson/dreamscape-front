b53125cb23cc33b9e167a4db09e43b17395850de
kametazin <0x0e0w@gmail.com>
2024-03-04 16:12:00 +0100

    fix mobile

--- src/app/components/Forms/Create.jsx
+++ src/app/components/Forms/Create.jsx
@@ -8,7 +8,7 @@ import axios from 'axios';
 const CreateDestination = () => {
 return (
     <section className='flex items-center justify-center pt-[144px] pb-[171px] desktop:pt-[150px] desktop:pb-[251px]'>
-    <div className='flex flex-row w-[733px] h-[509px] rounded-2xl border-4 border-light-yellow '>
+    <div className='flex flex-col md:flex-row lg:w-[733px] h-[509px] rounded-2xl border-4 border-light-yellow sm:p-6'>
     <div class=" items-center w-[300px] basis-1/2 p-4">
         <h2 className='text-m text-red font-bold pt-3 '>Crear destino</h2>
         <form /*onSubmit*/ className='border-t-2 border-red flex flex-col'>
--- src/app/components/Forms/Edit.jsx
+++ src/app/components/Forms/Edit.jsx
@@ -8,8 +8,8 @@ import axios from 'axios';
 const EditDestination = () => {
 return (
     <section className='flex items-center justify-center pt-[144px] pb-[171px] desktop:pt-[150px] desktop:pb-[251px]'>
-    <div className='flex flex-row w-[733px] h-[509px] rounded-2xl border-4 border-light-yellow '>
-    <div class=" items-center w-[300px] basis-1/2 p-4">
+    <div className='flex flex-col md:flex-row lg:w-[733px] h-[509px] rounded-2xl border-4 border-light-yellow sm:p-6'>
+    <div class=" items-center w-[300px] lg:basis-1/2 p-4 ">
         <h2 className='text-m text-red font-bold pt-3 '>Editar destino</h2>
         <form /*onSubmit*/ className='border-t-2 border-red flex flex-col'>
        <label for='titulo' className='text-blue text-s font-bold pb-1 pt-6'>Titulo</label>
@@ -21,7 +21,7 @@ return (
         <DoubleButton/>
     </form>
     </div>
-          <div class="basis-1/2 items-center w-[300px] mt-20">
+          <div class="desktop:basis-1/2 items-center w-[300px] mt-20">
           <label for='razon' className='text-blue text-s font-bold pb-1 pt-6'>¿Por qué quieres viajar allí?</label>
        <InputBox size='xl' placeholder="La explicación del porque quieres viajar allí antes  de 500 caracteres." id="razon" type="text" name="razon" /*value={formData.password} onChange={handleChange}*/ /> 
           </div>
