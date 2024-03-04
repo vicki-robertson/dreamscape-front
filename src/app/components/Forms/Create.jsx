ef4183f557e0de0b5f9b7b9b6c7578fc5353508b
kametazin <0x0e0w@gmail.com>
2024-03-04 11:21:31 +0100

    init create destination

---
+++ src/app/components/Forms/Create.jsx
@@ -0,0 +1,31 @@
+"use client"
+
+import React, { useState} from 'react';
+import InputBox from '../ui/InputBox';
+import DoubleButton from '../ui/DoubleButton';
+import axios from 'axios';
+
+const CreateDestination = () => {
+return (
+    <section className='flex items-center justify-center pt-[144px] pb-[171px] desktop:pt-[150px] desktop:pb-[251px]'>
+    <div className='flex flex-row w-[733px] h-[509px] rounded-2xl border-4 border-light-yellow '>
+    <div class=" items-center w-[300px] basis-1/2 ">
+        <h2 className='text-m text-red font-bold pt-3 '>Crear destino</h2>
+        <form /*onSubmit*/ className='border-t-2 border-red flex flex-col'>
+       <label for='titulo' className='text-blue text-s font-bold pb-1 pt-6'>Titulo</label>
+       <InputBox size='m' placeholder="Escribe titulo..." type="text" name="titulo" /*value={formData.email} onChange={handleChange}*//> 
+       <label for='Ubicación' className='text-blue text-s font-bold pb-1 pt-6'>Ubicación</label>
+       <InputBox size='m' placeholder="Escribe ubicación..." type="text" name="Ubicación" /*value={formData.password} onChange={handleChange}*/ /> 
+        <DoubleButton/>
+    </form>
+    </div>
+          <div class="basis-1/2 items-center w-[300px] mt-20">
+          <label for='razon' className='text-blue text-s font-bold pb-1 pt-6'>¿Por qué quieres viajar allí?</label>
+       <InputBox size='xl' placeholder="La explicación del porque quieres viajar allí antes  de 500 caracteres." type="text" name="razon" /*value={formData.password} onChange={handleChange}*/ /> 
+          </div>
+    </div>
+        </section>
+)
+}
+
+export default CreateDestination;
---
+++ src/app/create-dest/page.jsx
@@ -0,0 +1,11 @@
+import Header from '../components/Header/Header';
+import CreateDestination from '../components/Forms/Create'
+
+export default function createDest () {
+    return (
+        <>
+        <Header/>
+        <CreateDestination/>
+        </>
+    )
+}
