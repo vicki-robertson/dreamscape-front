5f43898315950f45ef31b62c4c4d18477b0d1665
kametazin <0x0e0w@gmail.com>
2024-03-04 16:21:21 +0100

    bad fix

--- src/app/components/Forms/Create.jsx
+++ src/app/components/Forms/Create.jsx
@@ -8,7 +8,7 @@ import axios from 'axios';
 const CreateDestination = () => {
 return (
     <section className='flex items-center justify-center pt-[144px] pb-[171px] desktop:pt-[150px] desktop:pb-[251px]'>
-    <div className='flex flex-col md:flex-row lg:w-[733px] h-[509px] rounded-2xl border-4 border-light-yellow sm:p-6'>
+    <div className='flex sm:flex-col md:flex-row desktop:w-[733px] h-[509px] rounded-2xl border-4 border-light-yellow p-6'>
     <div class=" items-center w-[300px] basis-1/2 p-4">
         <h2 className='text-m text-red font-bold pt-3 '>Crear destino</h2>
         <form /*onSubmit*/ className='border-t-2 border-red flex flex-col'>
@@ -21,7 +21,7 @@ return (
         <DoubleButton/>
     </form>
     </div>
-          <div class="basis-1/2 items-center w-[300px] mt-20">
+          <div class="basis-1/2 items-center desktop:w-[300px] mt-20">
           <label for='razon' className='text-blue text-s font-bold pb-1 pt-6'>¿Por qué quieres viajar allí?</label>
        <InputBox size='xl' placeholder="La explicación del porque quieres viajar allí antes  de 500 caracteres." id="razon" type="text" name="razon" /*value={formData.password} onChange={handleChange}*/ /> 
           </div>
