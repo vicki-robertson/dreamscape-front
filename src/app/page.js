e53431e2a2745b18ca3eae3a559d50f091d330f7
lolamindi <laura_mday@yahoo.co.uk>
2024-03-05 15:16:55 +0100

    begin process of connecting the api data to the front end

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,42 @@
-import Header from './components/Header/Header';
+"use client"
+
+import Header from '../app/components/Header/Header';
 import PhotoCard from './components/ui/PhotoCard';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+
 
 export default function Page() {
+const [destinations, setDestinations] = useState([]);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/');
+        setDestinations(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
   return (
     <>
       <Header/>
       <article className="flex justify-center items-center h-full">
         <div className="grid grid-cols-1 desktop:grid-cols-4 gap-x-6 gap-y-6 my-6 desktop:mx-16">
-          {[...Array(8).keys()].map((index) => (
-            <div key={index} className="flex justify-center">
-              <PhotoCard />
+          {destinations.map((destination) => (
+            <div key={destination.id} className="flex justify-center">
+              <PhotoCard data={destination} />
             </div>
           ))}
         </div>
       </article>
     </>
   )
-}
- 
+
+          }
