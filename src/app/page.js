8c2ff9d9848383df34dd46e9a4ef9ecd3cd5e466
debora-smb <debora.smborges@gmail.com>
2024-03-05 15:35:40 +0100

    add get all destinations logic

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,17 @@
 "use client"
-
-import Header from '../app/components/Header/Header';
+import Header from './components/Header/Header';
 import PhotoCard from './components/ui/PhotoCard';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-import {useEffect, useState} from 'react';
-
 
 export default function Page() {
-const [destinations, setDestinations] = useState([]);
-
+  const [destinos, setDestinos] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get('http://localhost:8000/api/');
-        setDestinations(response.data);
+        setDestinos(response.data.data);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -23,20 +20,19 @@ const [destinations, setDestinations] = useState([]);
     fetchData();
   }, []);
 
-
   return (
     <>
       <Header/>
       <article className="flex justify-center items-center h-full">
         <div className="grid grid-cols-1 desktop:grid-cols-4 gap-x-6 gap-y-6 my-6 desktop:mx-16">
-          {destinations.map((destination) => (
-            <div key={destination.id} className="flex justify-center">
-              <PhotoCard data={destination} />
+          {destinos.map((destino, index) => (
+            <div key={index} className="flex justify-center">
+              <PhotoCard data={destino} />
             </div>
           ))}
         </div>
       </article>
     </>
   )
-
-          }
+}
+ 
