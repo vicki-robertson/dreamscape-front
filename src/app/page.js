155914051bce735fcdcdfbb2482a299994a519d5
Jasz-17 <jaszapata1983@gmail.com>
2024-03-06 20:01:37 +0100

    implemented funcionality in pagination

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client"
 
 import Header from '../app/components/Header/Header';
+import PaginationButtons from './components/ui/PaginationButtons';
 import PhotoCard from './components/ui/PhotoCard';
 import axios from 'axios';
 import {useEffect, useState} from 'react';
@@ -8,21 +9,26 @@ import {useEffect, useState} from 'react';
 
 export default function Page() {
 const [destinations, setDestinations] = useState([]);
-
+const [currentPage, setCurrentPage] = useState(1);
+const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchData = async (page) => {
       try {
-        const response = await axios.get('http://localhost:8000/api/');
-        setDestinations(response.data);
+        const response = await axios.get(`http://localhost:8000/api/?page=${page}`);
+        setDestinations(response.data.data);
+        setTotalPages(response.data.last_page);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
 
-    fetchData();
-  }, []);
+    fetchData(currentPage);
+  }, [currentPage]);
 
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
   return (
     <>
@@ -35,6 +41,11 @@ const [destinations, setDestinations] = useState([]);
             </div>
           ))}
         </div>
+        <PaginationButtons
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
       </article>
     </>
   )
