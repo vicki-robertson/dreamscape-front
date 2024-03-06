fa9605a24eada1b75ea9a99f480d5e6f1090353f
debora-smb <debora.smborges@gmail.com>
2024-03-06 14:59:25 +0100

    added register form logic

--- src/app/components/Forms/RegisterForm.jsx
+++ src/app/components/Forms/RegisterForm.jsx
@@ -1,67 +1,78 @@
 "use client"
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 import InputBox from "../ui/InputBox";
 import Button from "../ui/Button";
 import axios from "axios";
 
-const Register = () => {
-
+export default function RegisterForm() {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: ''
+    name: "",
+    email: "",
+    password: ""
   });
-  
-  const handleChange = e => {
-    setFormData({ ...formData, [e.target.name]: e.target.value});
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const [loading, setLoading] = useState(false);
-
-  const handleCancel = () => {
-    setFormData({
-      name: '',
-      email: '',
-      password: ''
-    });
-    setLoading(false);
-  };
-
-  const handleSubmit = async e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:8000/api/register', formData);
-      console.log(response.data);
+      const response = await axios.post("http://localhost:8000/api/register", formData);
+      setMessage(response.data.message);
+      console.log(message)
     } catch (error) {
-      console.error(error.response.data);
+      setMessage("Error: " + error.response.data.message);
     }
   };
 
   return (
-    <div className="flex flex-col w-[370px] h-[487px] rounded-2xl border-4 items-center border-light-yellow ">
-      <h2 className="text-m text-red font-bold pt-3 ">Registro de usuario</h2>
+    <div className="flex flex-col w-[370px] h-[487px] rounded-2xl border-4 items-center border-light-yellow">
+      <h2 className="text-m text-red font-bold pt-3">Registro de usuario</h2>
       <form onSubmit={handleSubmit} className="border-t-2 border-red flex flex-col">
-        <label htmlFor='name' className='text-blue text-s font-bold pb-1 pt-6'>Nombre</label>
-        <InputBox size='m' placeholder="Escribe tu nombre..." onChange={handleChange} type="name" name="name"/> 
-       
-        <label htmlFor="email" className="text-blue text-s font-bold pb-1 pt-6" >
+        <label htmlFor="name" className="text-blue text-s font-bold pb-1 pt-6">
+          Nombre
+        </label>
+        <InputBox
+          size="m"
+          placeholder="Escribe tu nombre..."
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="email" className="text-blue text-s font-bold pb-1 pt-6">
           Email
         </label>
-        <InputBox size="m" placeholder="Escribe tu email..." onChange={handleChange} type="email" name="email" />
+        <InputBox
+          size="m"
+          placeholder="Escribe tu email..."
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
 
         <label htmlFor="password" className="text-blue text-s font-bold pb-1 pt-6">
           Contraseña
         </label>
-        <InputBox size="m" placeholder="Escribe tu contraseña..." onChange={handleChange} type="password" name="password" />
-      </form>
-      <div className="flex gap-4 justify-center pt-8">
+        <InputBox
+          size="m"
+          placeholder="Escribe tu contraseña..."
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <div className="flex gap-4 justify-center pt-8">
           <Button buttonColor="bg-green" buttonText="Aceptar" type="submit" />
-          <Button buttonColor="bg-red" buttonText="Cancelar" onClick={handleCancel} />
-      </div>
-      <span className="text-blue font-bold text-s pt-4">¿Ya tienes cuenta? Accede <a href="/login" className="text-green">aquí</a></span>
+          <Button buttonColor="bg-red" buttonText="Cancelar" />
+        </div>
+      </form>
     </div>
   );
 }
-
-export default Register;
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,12 +1,12 @@
 import Header from '../components/Header/Header'
-import SigninForm from '../components/Forms/RegisterForm'
+import RegisterForm from '../components/Forms/RegisterForm'
 
 export default function SignUpPage() {
   return (
     <>
         <Header/>
         <section className='flex flex-col items-center justify-center pt-[144px] pb-[171px] desktop:pt-[150px] desktop:pb-[251px]'>
-          <SigninForm/>
+          <RegisterForm/>
         </section>  
     </>
   )
