e57eb7261a322f8f2cc796e4d856e450b9e86ea3
lolamindi <laura_mday@yahoo.co.uk>
2024-03-06 12:43:12 +0100

    change styles to make photos align better

--- src/app/components/ui/PhotoCard.jsx
+++ src/app/components/ui/PhotoCard.jsx
@@ -5,13 +5,13 @@ function PhotoCard({data}) {
 
   return (
     <article className="h-[373px] w-[285px] relative">
-    <div className="overflow-hidden w-[285px] h-[300px] ring-4"> 
+    <div className="overflow-hidden w-[285px] h-[300px]"> 
       <Image
-        className="rounded-2xl left-0 top-0 h-100 object-cover"
+        className="rounded-2xl left-0 top-0 absolute z-20 h-[75%] object-cover"
         src={data.image}
         width={285}
         height={300}
-        alt={"representación del destino eligido"}
+        alt={data.name}
       />
     </div>
     <div className="flex flex-col h-[100px] w-[285px] top-[260px] bg-light-yellow rounded-br-[20px] rounded-bl-[20px] absolute">
