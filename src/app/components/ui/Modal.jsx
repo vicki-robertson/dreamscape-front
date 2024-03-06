abc8c42ff87c8956a96803feb919a131bb099912
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-03-06 12:13:15 +0100

    new photos added and style changes

--- src/app/components/ui/Modal.jsx
+++ src/app/components/ui/Modal.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import DoubleButton from './DoubleButton'
+import React from 'react';
+import Button from './Button';
+import {handleCancel} from '../Forms/LoginForm';
 
 export default function Modal() {
   return (
     <section className='w-80 h-[225px] desktop:h-[200px] desktop:w-[500px] bg-light-yellow rounded-3xl flex flex-col items-center justify-center'>
         <h3 className='text-m text-blue w-[200px] desktop:w-80 text-center pb-7'>¿Quieres eliminar este destino?</h3>
         <div>
-            <DoubleButton/>
+            <Button buttonColor="bg-green" buttonText="Aceptar" type="submit"/>
+            <Button buttonColor='bg-red' buttonText="Cancelar" onClick={handleCancel}/>
         </div>
     </section>
   )
