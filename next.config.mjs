bdfd8916810302568e549b8c69cf972649f46cf9
debora-smb <debora.smborges@gmail.com>
2024-03-07 15:06:51 +0100

    delete images folder and update next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+  const nextConfig = {
+    images: {
+      domains: ['localhost'],
+    },
+  };
+  
+  export default nextConfig;
