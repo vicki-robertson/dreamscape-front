af6b1ec560d7839fb0fd4a3c3232316960640ce8
lolamindi <laura_mday@yahoo.co.uk>
2024-03-11 15:17:49 +0100

    start to implement infinite scroll in mobile version of home page

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import PhotoCard from "./components/ui/PhotoCard";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import PaginationButtons from './components/ui/PaginationButtons';
+import useSWRInfinite from 'swr/infinite';
 
 export default function Page() {
   const [destinations, setDestinations] = useState([]);
@@ -13,21 +14,54 @@ export default function Page() {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [error, setError] = useState("");
+  const isMobile = window.innerWidth <= 1000;
 
+  const getKey = (pageIndex, previousPageData) => {
+    return isMobile ? `http://localhost:8000/api/?page=${pageIndex + 1}` : null;
+  };
+
+  const { data, error: swrError, size, setSize } = useSWRInfinite(getKey);
 
   useEffect(() => {
-    const fetchData = async (page) => {
-      try {
-        console.log('Fetching data for page:', page);
-        const response = await axios.get(`http://localhost:8000/api/?page=${page}`);
-        setDestinations(response.data.data);
-        setTotalPages(response.data.last_page);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
+    if (!isMobile) {
+      setSize(1);
+    }
+  }, [isMobile, setSize]);
+
+  const fetchData = async (page) => {
+    try {
+      console.log('Fetching data for page:', page);
+      const response = await axios.get(`http://localhost:8000/api/?page=${page}`);
+      return response.data.data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    const loadMoreData = async () => {
+      const newDestinations = await fetchData(size);
+      setDestinations((prevDestinations) => [...prevDestinations, ...newDestinations]);
     };
 
-    fetchData(currentPage);
+    if (isMobile && size > 1) {
+      loadMoreData();
+    } else {
+      setDestinations([]);
+      loadMoreData();
+    }
+  }, [size, isMobile]);
+
+  useEffect(() => {
+    const initialData = async () => {
+      const newDestinations = await fetchData(currentPage);
+      setDestinations(newDestinations);
+      const response = await axios.get(`http://localhost:8000/api/?page=${currentPage}`);
+      setTotalPages(response.data.last_page);
+    };
+
+    initialData();
   }, [currentPage]);
 
   const handlePageChange = (page) => {
@@ -35,6 +69,7 @@ export default function Page() {
     setCurrentPage(page);
   };
 
+  const swrErrorMessage = swrError ? swrError.message : "";
 
   const handleSearch = async (searchTerm) => {
     setSearchInput(searchTerm);
@@ -54,11 +89,11 @@ export default function Page() {
 
   return (
     <>
-      <Header onSearch={handleSearch} showSearchBar={true}/>
+      <Header onSearch={handleSearch} showSearchBar={true} />
       <article className="flex flex-col justify-center items-center h-full">
         <div className="grid grid-cols-1 desktop:grid-cols-4 gap-x-6 gap-y-6 my-6 desktop:mx-16">
           {searchInput !== "" && searchResults.length === 0 ? (
-            <p className="text-blue">{error}</p>
+            <p className="text-blue">{error || swrErrorMessage}</p>
           ) : (
             (searchInput === "" ? destinations : searchResults).map(
               (destination, index) => (
@@ -70,10 +105,10 @@ export default function Page() {
           )}
         </div>
         <PaginationButtons
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={handlePageChange}
-      />
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
       </article>
     </>
   );
