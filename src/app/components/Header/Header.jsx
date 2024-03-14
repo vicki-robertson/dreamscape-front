1c6dba9ed27dbfb527a5d08e39f65c11f4167b96
debora-smb <debora.smborges@gmail.com>
2024-03-14 22:36:06 +0100

    added logout functionality

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -21,7 +21,7 @@ export default function Header({ onSearch, showSearchBar }) {
         <>
             <header className="flex mobile:flex-col mobile:items-center mobile:pt-[21px] desktop:flex-row desktop:justify-between desktop:mx-[70px] desktop:border-b-2 border-blue">
                 <Logo/>
-                <li className="mobile:pt-4 list-none desktop:flex pb-4">
+                <li className="mobile:pt-4 list-none desktop:flex pb-4 gap-3">
                     {showSearchBar && <SearchBar onSearch={onSearch}/>}
                     <div className="mobile:hidden desktop:flex gap-3">
                         <HomeButton />
